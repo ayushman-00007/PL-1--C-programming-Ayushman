09195e57dff0d189a5f68ccfe10272e1c7f39a0c
ayushman-00007 <ayusansingh@gmail.com>
2026-08-30 23:13:13 +0530

    Create ayush_P5.C

---
+++ ayush_P5.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 33;
+    int b = 47;
+    int c = 20;
+
+    float average;
+
+    average = (a + b + c) / 3.0;
+
+    printf("\nAverage = %f", average);
+
+    return 0;
+}
